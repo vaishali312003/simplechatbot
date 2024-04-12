25c66e40ee0d7611a80ca53fc6756c516f4a43f3
heroic31 <vagupta310@gmail.com>
2024-04-12 15:46:08 +0530

    initial commit

---
+++ model.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def chat():
+    data = request.json
+    message = data['message'].lower()
+
+    # Simple responses based on keywords
+    if 'hello' in message:
+        response = "Hi there!"
+    elif 'how are you' in message:
+        response = "I'm doing well, thank you!"
+    elif 'bye' in message:
+        response = "Goodbye! Take care."
+    else:
+        # If no keyword detected, return a random response
+        responses = ["I'm not sure I understand.", "Could you please rephrase that?", "Sorry, I didn't get that."]
+        response = random.choice(responses)
+
+    return jsonify({'message': response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
